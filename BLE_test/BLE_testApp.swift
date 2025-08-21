675312ce1e45370dd6b022078913c1c2d0e1613d
RikuRaito <sairikuneont@gmail.com>
2025-08-21 12:22:21 +1000

    Initial Commit

---
+++ BLE_test/BLE_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_testApp.swift
+//  BLE_test
+//
+//  Created by 斉藤吏功 on 2025/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BLE_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
